557391a9717bdcc86ac652c1e449e5af3670effa
Poom Pattarapon <ppst.801@gmail.com>
2020-08-13 14:23:02 +0700

    migrated

--- restfulapicrud/restfulapicrud/settings.py
+++ restfulapicrud/restfulapicrud/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'restfulapicrud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'web_app',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
     }
 }
 
